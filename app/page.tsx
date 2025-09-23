4e03abc2054ea5a6a5e7bfef2bcab4af323c0deb
remilanova <r.e.milanova@gmail.com>
2025-09-23 03:53:17 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+  return (
+    <div className="card">
+      <h1>Shentu – Портал</h1>
+      <p>Тук ще има страници, блог и затворена зона за абонати.</p>
+    </div>
+  )
+}
