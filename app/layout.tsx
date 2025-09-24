288fd097a2cfa0b6ba58fb350bd3b3aed57ad644
remilanova <r.e.milanova@gmail.com>
2025-09-24 13:29:10 +0300

    sync

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: 'Shentu' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="bg">
-      <body style={{ fontFamily: 'system-ui', padding: 24 }}>{children}</body>
-    </html>
-  )
-}
